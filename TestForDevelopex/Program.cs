d58cb9c14e0adde62d28a63ea67881f457f4939c
Illia Lubenets <illialubenets@yandex.ua>
2016-01-11 18:11:02 +0200

    Ready

---
+++ TestForDevelopex/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TestForDevelopex
+{
+    class Program
+    {
+        static int N = 10000;
+        static void GenerateList()
+        {
+            Random rand = new Random();
+            int N = 10000;
+            List<int> Numbers = new List<int>();
+            for(int i=0;i< N;i++)
+            {
+                Numbers.Add(i);
+            }
+            using (StreamWriter sw = new StreamWriter("out.txt"))
+            {
+                while (N > 0)
+                {
+                    int i = rand.Next() % N;
+                    int k = Numbers[i];
+                    sw.Write("{0}, ", k);
+                    Numbers.RemoveAt(i);
+                    N--;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            GenerateList();
+        }
+    }
+}
